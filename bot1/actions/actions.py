c303de51ece2abdab192f3a41fbeaff9a3ee0746
goldberl <goldberl@dickinson.edu>
2022-09-23 11:54:41 -0400

    Implemented single stage fallback

--- bot1/actions/actions.py
+++ bot1/actions/actions.py
@@ -12,7 +12,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+from rasa_sdk.events import UserUtteranceReverted
 
 class ActionCheckNumQuestions(Action):
 
@@ -45,44 +45,69 @@ class ActionCheckNumQuestions(Action):
         return [SlotSet("quesAsked", num_q)]
 
 
-
 class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
 
-    def name(self):
-        return "action_default_ask_affirmation"
+    def name(self) -> Text:
+        return "action_default_fallback"
 
-    async def run(self, dispatcher, tracker, domain):
-            # select the top three intents from the tracker
-            # ignore the first one -- nlu fallback
-            predicted_intents = tracker.latest_message["intent_ranking"][1:4]
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("Sorry, can you rephrase that message?")
 
-            # A prompt asking the user to select an option
-            message = "Sorry! What do you want to do?"
+        # tell the user they are being passed to a customer service agent
+        dispatcher.utter_message(text="I am passing you to a human...")
 
-            # a mapping between intents and user friendly wordings
-            intent_mappings = {
-                "inform_name": "Tell me your name",
-                "greet": "Agree",
-                "goodbye": "End conversation"
-            }
+        # assume there's a function to call customer service
+        # pass the tracker so that the agent has a record of the conversation between the user
+        # and the bot for context
+        call_customer_service(tracker)
 
-            # show the top three intents as buttons to the user
-            buttons = [
-                {
-                    "title": intent_mappings[intent['name']],
-                    "payload": "/{}".format(intent['name'])
-                }
-                for intent in predicted_intents
-            ]
+        # pause the tracker so that the bot stops responding to user input
+        return [ConversationPaused(), UserUtteranceReverted()]
 
-            # add a "none of these button", if the user doesn't
-            # agree when any suggestion
-            buttons.append({
-                "title": "None of These",
-                "payload": "/out_of_scope"
-            })
-    dispatcher.utter_message(text=message, buttons=buttons)
-    return []
+# # class ActionDefaultFallback(Action):
+#
+#     def name(self):
+#         return "action_default_ask_affirmation"
+#
+#     async def run(self, dispatcher, tracker, domain):
+#         # select the top three intents from the tracker
+#         # ignore the first one -- nlu fallback
+#         predicted_intents = tracker.latest_message["intent_ranking"][1:4]
+#
+#         # A prompt asking the user to select an option
+#         message = "Sorry! What do you want to do?"
+#
+#         # a mapping between intents and user friendly wordings
+#         intent_mappings = {
+#             "inform_name": "Tell me your name",
+#             "greet": "Agree",
+#             "goodbye": "End conversation"
+#         }
+#
+#         # show the top three intents as buttons to the user
+#         buttons = [
+#             {
+#                 "title": intent_mappings[intent['name']],
+#                 "payload": "/{}".format(intent['name'])
+#             }
+#             for intent in predicted_intents
+#         ]
+#
+#         # add a "none of these button", if the user doesn't
+#         # agree when any suggestion
+#         buttons.append({
+#             "title": "None of These",
+#             "payload": "/out_of_scope"
+#         })
+#         dispatcher.utter_message(text=message, buttons=buttons)
+#         return []
 
 # class ActionTest(Action):
 #
